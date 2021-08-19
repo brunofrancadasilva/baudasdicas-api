8bec7f62ced8c7d5118e117e174b3eb4b7f373d2
Bruno França <bruno.franca@deskfy.io>
2021-08-19 19:30:52 -0300

    Added morgan

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const CONSTANTS = require('./app/config/constants');
 const db = require('./app/models/index');
 const ApiManager = require('./app/managers/apiManager');
