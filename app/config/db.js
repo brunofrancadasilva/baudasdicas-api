09d821a5d3b35ea3bda0eb586e8a259946817edf
Bruno França <bruno.franca@deskfy.io>
2021-08-19 12:04:56 -0300

    Changed db connection source

--- app/config/db.js
+++ app/config/db.js
@@ -2,11 +2,11 @@
 const CONSTANTS = require('./constants');
 
 const db = {
-  DB_HOST: CONSTANTS.DB_HOST,
-  DB_USER: CONSTANTS.DB_USER,
-  DB_PASSWORD: CONSTANTS.DB_PASSWORD,
-  DB_PORT: CONSTANTS.DB_PORT,
-  DB_NAME: CONSTANTS.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
   dialect: 'postgres',
   schema: 'public',
   isolationLevel: CONSTANTS.DB_ISOLATION_LEVEL,
--- database.js
+++ database.js
@@ -4,32 +4,32 @@ const CONSTANTS = require('./app/config/constants');
 
 module.exports = {
   development: {
-    DB_USER: CONSTANTS.DB_USER,
-    DB_PASSWORD: CONSTANTS.DB_PASSWORD,
-    DB_NAME: CONSTANTS.DB_NAME,
-    DB_HOST: CONSTANTS.DB_HOST,
-    DB_PORT: CONSTANTS.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
     dialect: 'postgres',
     schema: 'public',
     isolationLevel: CONSTANTS.DB_ISOLATION_LEVEL,
     pool: {
-      max: 25,
+      max: 22,
       min: 0,
       idle: 10000,
       acquire: 30000
     },
   },
   production: {
-    DB_USER: CONSTANTS.DB_USER,
-    DB_PASSWORD: CONSTANTS.DB_PASSWORD,
-    DB_NAME: CONSTANTS.DB_NAME,
-    DB_HOST: CONSTANTS.DB_HOST,
-    DB_PORT: CONSTANTS.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
     dialect: 'postgres',
     schema: 'public',
     isolationLevel: CONSTANTS.DB_ISOLATION_LEVEL,
     pool: {
-      max: 25,
+      max: 22,
       min: 0,
       idle: 10000,
       acquire: 30000
